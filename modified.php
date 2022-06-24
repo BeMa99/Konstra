312da51803590936cbbc8d97c8cbe42494914db4
berclay <berclaym@gmail.com>
2022-06-24 14:52:07 +0300

    changed modified.php file

--- modified.php
+++ modified.php
@@ -6,8 +6,16 @@ if (!isset($_POST['Submit']))
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$query = "INSERT INTO suggestions (fname, email, message) VALUES ('$fname','$email','$message')";
-	if (mysqli_query($conn, $query)){echo "Message Inserted Successfully!"; echo $_SERVER[HTTP_REFERER];}
+	if (mysqli_query($conn, $query)){echo "Message Inserted Successfully!";}
 	else{echo "Error: " . $sql . ":-" . mysqli_error($conn);}
+	
+	if(isset($_REQUEST["destination"]))
+	{
+      header("Location: {$_REQUEST["destination"]}");
+	}else if(isset($_SERVER["HTTP_REFERER"]))
+	{
+      header("Location: {$_SERVER["HTTP_REFERER"]}");
+	}
 	mysqli_close($conn);
 }
 ?>
